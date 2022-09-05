92f4c7783cd1c16aab3eda6719d26f2befe8a4db
Helena Johansson <helena@klarr.se>
2022-09-05 12:05:47 +0200

    code from todays lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Header from "./components/Header";
+import AboutPage from "./components/AboutPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+      </div>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/about/:pageId" element={<AboutPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+// top level ska alltid vara browserrouter
+// path=/ är alltid default alltså http://localhost:3000 egen domän va =
+// http://klarr.se
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <div style={{ marginTop: "10px" }}></div>
+    </div>
+  );
+};
+
+export default About;
